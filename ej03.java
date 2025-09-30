32414253d111da43ed42e4286fab3f76b2313c77
cuevasjr1 <yeray.cuevas.martin.al@iespoligonosur.org>
2025-09-30 17:17:05 +0000

    Ejercicio 3

---
+++ ej03.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ej03 {
+    public static void main(String[] args) {
+        System.out.println("Dime cuantos años tienes");
+        Scanner sc = new Scanner(System.in);
+        int edad = sc.nextInt();
+        System.out.println("Tienes edad años");
+    }
+}
