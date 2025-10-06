1099d9d790194e71f3a8763b6f7452fc35116d59
cuevasjr1 <yeray.cuevas.martin.al@iespoligonosur.org>
2025-10-06 17:58:40 +0000

    ej09 terminado

---
+++ ej09.java
@@ -0,0 +1,8 @@
+public class ej09 {
+    public static void main(String[] args) {
+        int globosExplotados = ;
+        System.out.println("El numero de globos explotados es " + globosExplotados);
+        //Salta un error al intentar ejecutar el codigo
+
+    }
+}
