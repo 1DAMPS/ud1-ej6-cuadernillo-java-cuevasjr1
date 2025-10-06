fdb61dfcca74eabdff72674dc8a7df1c5c0fba86
cuevasjr1 <yeray.cuevas.martin.al@iespoligonosur.org>
2025-10-06 17:07:51 +0000

    ej06 terminado

---
+++ ej06.java
@@ -0,0 +1,10 @@
+public class ej06 {
+    public static void main(String[] args) {
+        char inicialDeTuNombre = 'Y';
+        double estaturaDelUsuario = 1.73;
+        int edadDelUsuario = 20;
+        System.out.println("Tus inicial es " + inicialDeTuNombre);
+        System.out.println("Tu estatura es " + estaturaDelUsuario);
+        System.out.println("Tu edad es " + edadDelUsuario);
+    }
+}
