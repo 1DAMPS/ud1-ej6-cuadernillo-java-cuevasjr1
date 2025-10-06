52dc5a23e409df7db473562dc2e16d138362f388
cuevasjr1 <yeray.cuevas.martin.al@iespoligonosur.org>
2025-10-06 17:46:37 +0000

    ej08 terminado

---
+++ ej08.java
@@ -0,0 +1,12 @@
+public class ej08 {
+    public static void main(String[] args) {
+        int librosLeidosAlMes = 5;
+        double dineroGastadoEnLibros = 84.99;
+        char estadoDelLibro = 'A';
+        boolean fueronBuenosLibros = true;
+        System.out.println("Has leido " + librosLeidosAlMes + " al mes." );
+        System.out.println("Has gastado " + dineroGastadoEnLibros + " en libros");
+        System.out.println("El estado del libro fue categoria " + estadoDelLibro);
+        System.out.println("El libro fue bueno? " + fueronBuenosLibros);
+    }
+}
