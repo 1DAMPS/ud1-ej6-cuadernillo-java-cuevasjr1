d6a92bab54d5a2b17055fb70294a9f1ea0d12541
cuevasjr1 <yeray.cuevas.martin.al@iespoligonosur.org>
2025-10-07 16:58:17 +0000

    ej11 terminado

---
+++ ej11.java
@@ -0,0 +1,6 @@
+public class ej11 {
+    public static void main(String[] args) {
+        long numeroGrande = 800000000;
+        System.out.println("El numero de poblacion actualmente es de " + numeroGrande);
+    }
+}
