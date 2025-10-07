8746be1fc00d6a4c2dc173390f7f7d391b5b69cb
cuevasjr1 <yeray.cuevas.martin.al@iespoligonosur.org>
2025-10-07 16:49:18 +0000

    ej10 terminado

---
+++ ej10.java
@@ -0,0 +1,8 @@
+public class ej10 {
+    public static void main (String[] args) {
+        byte numero = 126;
+        short numero2  = -32767;
+        System.out.println("El byte introducido es " + numero);
+        System.out.println("El short introducido es " + numero2);
+    }
+}
